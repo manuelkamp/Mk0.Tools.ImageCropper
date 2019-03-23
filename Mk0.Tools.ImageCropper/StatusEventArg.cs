cb0b2954e53d2010e979f6c5e16c4af11602f26d
Manuel Kamper <manuelk@outlook.com>
2019-03-23 15:00:30 +0100

    Initial

---
+++ Mk0.Tools.ImageCropper/StatusEventArg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace Mk0.Tools.ImageCropper
+{
+    /// <summary>
+    /// Passes the cropping rectangle and current cursor position to the host object
+    /// </summary>
+    public class StatusEventArg : EventArgs
+    {
+        public readonly Rectangle CroppingRect;
+        public readonly Point CursorPos;
+
+        public StatusEventArg(Point pos, Rectangle crop)
+        {
+            CursorPos = pos;
+            CroppingRect = crop;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Mk0.Tools.ImageCropper
+(C) 2019 mk0.at
+
+This Tool provides a new form control to crop images.
